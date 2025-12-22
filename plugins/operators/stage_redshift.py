726a17a5fccdcc73e8b59b392b3e6f36875986ad
vzeufack <43120988+vzeufack@users.noreply.github.com>
2025-12-22 11:55:32 -0500

    Add data staging from S3 to Redshift
    Implemented the execute method to copy data from S3 to Redshift, including clearing the destination table and handling AWS credentials.

--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -4,23 +4,58 @@ from airflow.utils.decorators import apply_defaults
 
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
+    template_fields = ("s3_key",)
+    copy_sql = """
+        COPY {}
+        FROM '{}'
+        ACCESS_KEY_ID '{}'
+        SECRET_ACCESS_KEY '{}'
+        IGNOREHEADER {}
+        DELIMITER '{}'
+        FORMAT AS JSON '{}'
+    """
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
+                 redshift_conn_id="redshift",
+                 aws_credentials_id="aws_credentials",
+                 table="",
+                 s3_bucket="dend-vannel",
+                 s3_key="",
+                 delimiter=",",
+                 json_format="auto",
+                 ignore_headers=1,
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.table = table
+        self.redshift_conn_id = redshift_conn_id
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.delimiter = delimiter
+        self.ignore_headers = ignore_headers
+        self.aws_credentials_id = aws_credentials_id
+        self.json_format = json_format
 
     def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
+        metastoreBackend = MetastoreBackend()
+        aws_connection=metastoreBackend.get_connection(self.aws_credentials_id)
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
 
+        self.log.info("Clearing data from destination Redshift table")
+        redshift.run("DELETE FROM {}".format(self.table))
 
-
-
-
+        self.log.info("Copying data from S3 to Redshift")
+        rendered_key = self.s3_key.format(**context)
+        s3_path = "s3://{}/{}".format(self.s3_bucket, rendered_key)
+        format_as_json = "auto" if self.json_format == "auto" else "s3://{}/{}".format(self.s3_bucket, self.json_format)
+        formatted_sql = S3ToRedshiftOperator.copy_sql.format(
+            self.table,
+            s3_path,
+            aws_connection.login,
+            aws_connection.password,
+            self.ignore_headers,
+            self.delimiter,
+            format_as_json
+        )
+        redshift.run(formatted_sql)
