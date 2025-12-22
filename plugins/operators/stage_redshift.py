5cd5fef06865af1bddc5e77c66c308551847cc6b
vzeufack <43120988+vzeufack@users.noreply.github.com>
2025-12-22 15:51:27 -0500

    Refactor StageToRedshiftOperator parameters
    Refactor StageToRedshiftOperator to remove unused parameters and add MetastoreBackend import.

--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,6 +1,7 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from airflow.secrets.metastore import MetastoreBackend
 
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
@@ -10,21 +11,18 @@ class StageToRedshiftOperator(BaseOperator):
         FROM '{}'
         ACCESS_KEY_ID '{}'
         SECRET_ACCESS_KEY '{}'
-        IGNOREHEADER {}
-        DELIMITER '{}'
         FORMAT AS JSON '{}'
+        REGION 'us-west-2'
     """
 
     @apply_defaults
     def __init__(self,
+                 table="",
+                 s3_key="",
                  redshift_conn_id="redshift",
                  aws_credentials_id="aws_credentials",
-                 table="",
                  s3_bucket="dend-vannel",
-                 s3_key="",
-                 delimiter=",",
                  json_format="auto",
-                 ignore_headers=1,
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
@@ -32,8 +30,6 @@ class StageToRedshiftOperator(BaseOperator):
         self.redshift_conn_id = redshift_conn_id
         self.s3_bucket = s3_bucket
         self.s3_key = s3_key
-        self.delimiter = delimiter
-        self.ignore_headers = ignore_headers
         self.aws_credentials_id = aws_credentials_id
         self.json_format = json_format
 
@@ -49,13 +45,11 @@ class StageToRedshiftOperator(BaseOperator):
         rendered_key = self.s3_key.format(**context)
         s3_path = "s3://{}/{}".format(self.s3_bucket, rendered_key)
         format_as_json = "auto" if self.json_format == "auto" else "s3://{}/{}".format(self.s3_bucket, self.json_format)
-        formatted_sql = S3ToRedshiftOperator.copy_sql.format(
+        formatted_sql = StageToRedshiftOperator.copy_sql.format(
             self.table,
             s3_path,
             aws_connection.login,
             aws_connection.password,
-            self.ignore_headers,
-            self.delimiter,
             format_as_json
         )
         redshift.run(formatted_sql)
