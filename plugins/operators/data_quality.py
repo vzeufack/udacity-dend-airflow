e910852fa97e0c7c1b651fd4b8bc0b26c62da8e0
vzeufack <43120988+vzeufack@users.noreply.github.com>
2025-12-22 11:56:57 -0500

    Add data quality checks in DataQualityOperator
    Implement data quality checks for specified tables in Redshift.

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -8,15 +8,30 @@ class DataQualityOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id: "redshift",
+                 tables: list,
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.tables = tables
 
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        for table in self.tables:
+            self.log.info(f"Running data quality check on table: {table}")
+
+            records = redshift.get_records(f"SELECT COUNT(*) FROM {table}")
+
+            if len(records) < 1 or len(records[0]) < 1:
+                raise ValueError(f"Data quality check failed: No results returned for table {table}")
+
+            row_count = records[0][0]
+
+            if row_count < 1:
+                raise ValueError(f"Data quality check failed: Table {table} contains 0 rows")
+
+            self.log.info(f"Table {table} passed row check with {row_count} rows.")
+        
+        self.log.info("All data quality checks passed successfully.")
