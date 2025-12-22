25388393abc12b07cf363ea7d2d2af542d963e69
vzeufack <43120988+vzeufack@users.noreply.github.com>
2025-12-22 11:56:28 -0500

    Implement LoadDimensionOperator with SQL execution

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -8,15 +8,31 @@ class LoadDimensionOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id: "redshift",
+                 target_table: "",
+                 sql_statement: "",
+                 truncate_insert: True,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.target_table = target_table
+        self.sql_statement = sql_statement
+        self.truncate_insert = truncate_insert
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        if self.truncate_insert:
+            self.log.info(f"Truncating Redshift table {self.target_table}")
+            redshift.run(f"TRUNCATE TABLE {self.target_table}")
+
+        formatted_sql = f"""
+            INSERT INTO {self.target_table}
+            {self.sql_statement}
+        """
+
+        self.log.info(f"Loading data into Redshift table {self.target_table}")
+        redshift.run(formatted_sql)
+
+        self.log.info(f"Load into {self.target_table} completed successfully.")
